e48e7eef9fcc730901ce564bd69f080fc2e4f51c
king3212 <2591579996@qq.com>
2024-03-12 13:04:21 +0800

    write the main function

---
+++ lab1/classfier.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<regex>
+#include<string>
+
+using namespace std;
+
+enum Words{Keyword, Identifiers, Literals};
+
+vector<pair<string,Words>> classfier(string line){
+    
+}

3ddc4173b5ae7b09e1c8d969d34894a81e8329b1
King3212 <116470803+King3212@users.noreply.github.com>
2024-03-26 19:30:07 +0800

    update test

--- lab1/CLI-TEST/test/testscanner.cpp
+++ lab1/CLI-TEST/test/testscanner.cpp
@@ -14,19 +14,74 @@ void testFile(string filename,TOKEN test){
     for (auto i : result)
     {
         assert(i.second == test);
+    }
+    
+}
+void testFile(string filename){
+    vector<string> lines = readFile(filename);
+    vector<pair<string,TOKEN>> result;
+    for(string i : lines){
+        assert(readLine(i,result) == NoError);
+    }
+    for (auto i : result)
+    {
         cout << i.first << " " << getToken(i.second) << endl;
     }
     
 }
+
+
+void testLongFile(string filename){
+    vector<string> lines = readFile(filename);
+    vector<pair<string,TOKEN>> *result = new vector<pair<string,TOKEN>>();
+    for(string i : lines){
+        for (int j = 0; j < 1e5; j++)
+        {
+            assert(readLine(i,*result) == NoError);
+        }
+
+    }
+    assert(result->size() == lines.size()*1e5);
+    delete result;
+}
+
+
 int charToInt(char x){
     assert(isdigit(x));
     return x-'0';
 }
 int main(int argc, char *argv[]){
-    assert(argc == 1);
+    string filename;
+    assert(argc == 0 || argc == 1);
+    vector<string> testTarget = {
+        "testKeywordData.txt","testDelimiterData.txt",
+        "testIdentifierData.txt","testOperatorData.txt",
+        "testIntergerData.txt","testStringData.txt",
+        "testFloatData.txt","testCommentData.txt",
+        "testSpecial_symbolData.txt","testCharData.txt"
+    };
 
-    vector<string> testTarget = {"testCommentData.cpp","testKeywordData.cpp","testIntergerData.cpp","testStringData.cpp"};
-    string filename = "testData/"+testTarget[charToInt(argv[0][0])];
-    testFile(filename,TOKEN(charToInt(argv[0][0])));
+
     
+
+
+    if (argc == 1)
+    {
+        assert(argv[0][0] < 10);
+        filename = "./test/testData/"+testTarget[charToInt(argv[0][0])];
+        testFile(filename,TOKEN(charToInt(argv[0][0])));
+    }else{
+        for (int i = 0; i < testTarget.size(); i++)
+        {
+            filename = "./test/testData/"+testTarget[i];
+            testFile(filename,TOKEN(i));
+        }
+        cout << "Union test pass!\n";
+        filename = "./test/testData/testdata.txt";
+        testFile(filename);
+        cout << "Normal test pass!\n";
+        testLongFile(filename);
+        cout << "Long test pass!";
+
+    }
 }

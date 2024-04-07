5862c7ca2de47a68513441d08f41029732691404
king3212 <2591579996@qq.com>
2024-04-07 22:22:38 +0800

    works

--- lab2/Gragh/scanner.cpp
+++ lab2/Gragh/scanner.cpp
@@ -1,28 +1,76 @@
 #include<vector>
 #include<string>
+#include"scanner.h"
 #include<map>
+
 /**
  * 这里扫描所有的句子
  * 
  * 将等号前面与等号后面的部分做扫描
- * 确定一一对应的正则表达式标识符
+ * 获取标识符以及一一对应的正则表达式
+ *
+ *  例如:
+ * 
+ * input:
+ * >>> number = [0-9]
+ * output:
+ * >>> result:{
+ *     name = "number"
+ *     needToScan = true
+ *     re = "[0-9]"
+ * }
 */
-std::vector<std::pair<std::string,std::string>> preScanner(std::vector<std::string> lines){
-    for (auto line : lines)
+std::vector<scanData*> *scanAll(std::vector<std::string> lines){
+
+    std::vector<scanData*> *result = new std::vector<scanData*>();
+    scanData *oneData = nullptr;
+    int startPos = 0;
+
+    for (std::string line : lines)
     {
+        oneData = new scanData(); 
         int pos = 0;
+        startPos = 0;
+        oneData->needToScan = false;
         while (true)
         {
             if (line[pos] == ' ' || line[pos] == '\t')
             {
                 pos++;
+            }else if(startPos == 0){
+                startPos = pos;
+                if (line[pos] == '_')
+                {
+                    oneData->needToScan = true;
+                }
+                pos++;
+            }else if(line[pos] == '='){
+                oneData->name = line.substr(startPos,pos-startPos);
+                oneData->re = line.substr(pos+1);
+                break;
             }else{
-                
+                pos++;
             }
-            
+
         }
-        
+        result->push_back(oneData);
     }
-    
-    
+    return result;
+}
+
+/**
+ * 此函数进行替换扫描
+ * 从最后一行开始往前搜索所有字符串是否被定义
+ * 如果被定义就替换为相应的正则表达式
+ * 
+ * 例如：
+ * 
+ * input:
+ * >>> number = [0-9]
+ * >>> _numbers = number*
+ * output:
+ * >>> _numbers = ([0-9])*
+*/
+std::vector<finalData> replaceRe(std::vector<scanData*> reExpresses){
+
 }
--- lab2/Gragh/scanner.h
+++ lab2/Gragh/scanner.h
@@ -18,6 +18,7 @@ struct finalData
 
 /** * 这个函数处理一个文件中的所有正则表达式
  * 对定义的临时变量进行替换
+ * 将高级运算转化为基础三种运算
  * 最终返回需要转换DFA的名称及其正则表达式
 */
-finalData scanner(const std::vector<std::string> lines);
+std::vector<finalData> scanner(std::vector<std::string> lines);

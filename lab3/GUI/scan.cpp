4795f026b77bb024d827c898385d0cef0492281e
King3212 <King321266@gmail.com>
2024-05-29 22:05:47 +0800

    something upload

---
+++ lab3/GUI/scan.cpp
@@ -0,0 +1,274 @@
+﻿#pragma execution_character_set("utf-8")
+/****************************************************/
+/* File: scan.c                                     */
+/* The scanner implementation for the TINY compiler */
+/* Compiler Construction: Principles and Practice   */
+/* Kenneth C. Louden                                */
+/****************************************************/
+
+#include "globals.h"
+#include "util.h"
+#include "scan.h"
+
+/* states in scanner DFA */
+typedef enum
+   { START,INASSIGN,INCOMMENT,INNUM,INID,DONE,INC,DEC,REEQ,RTS,LTS }
+   StateType;
+
+/* lexeme of identifier or reserved word */
+char tokenString[MAXTOKENLEN+1];
+
+
+/* getNextChar fetches the next non-blank character
+   from lineBuf, reading in a new line if lineBuf is
+   exhausted */
+static int getNextChar(void)
+{ if (!(linepos < bufsize))
+  { lineno++;
+    if (fgets(lineBuf,BUFLEN-1,source))
+    { if (EchoSource) fprintf(listing,"%4d: %s",lineno,lineBuf);
+      bufsize = strlen(lineBuf);
+      linepos = 0;
+      return lineBuf[linepos++];
+    }
+    else
+    { EOF_flag = TRUE;
+      return EOF;
+    }
+  }
+  else return lineBuf[linepos++];
+}
+
+/* ungetNextChar backtracks one character
+   in lineBuf */
+static void ungetNextChar(void)
+{ if (!EOF_flag) linepos-- ;}
+
+/* lookup table of reserved words */
+static struct
+    { char* str;
+      TokenType tok;
+    } reservedWords[MAXRESERVED]
+   = {{(char*)"if",IF},{(char*)"then",THEN},{(char*)"else",ELSE},{(char*)"end",END},
+      {(char*)"repeat",REPEAT},{(char*)"until",UNTIL},{(char*)"read",READ},
+      {(char*)"write",WRITE},{(char*)"while",WHILE},{(char*)"endwhile",ENDWHILE},{(char*)"for",FOR}};
+
+/* lookup an identifier to see if it is a reserved word */
+/* uses linear search */
+static TokenType reservedLookup (char * s)
+{ int i;
+  for (i=0;i<MAXRESERVED;i++)
+    if (!strcmp(s,reservedWords[i].str))
+      return reservedWords[i].tok;
+  return ID;
+}
+
+/****************************************/
+/* the primary function of the scanner  */
+/****************************************/
+/* function getToken returns the 
+ * next token in source file
+ */
+TokenType getToken(void)//获取token
+{  /* index for storing into tokenString */
+   int tokenStringIndex = 0;
+   /* holds current token to be returned */
+   TokenType currentToken;
+   /* current state - always begins at START */
+   StateType state = START;
+   /* flag to indicate save to tokenString */
+   int save;
+   while (state != DONE)
+   { int c = getNextChar();
+     save = TRUE;
+     switch (state)
+     { case START:
+         if (isdigit(c))
+           state = INNUM;
+         else if (isalpha(c))
+           state = INID;
+         else if (c == ':')
+           state = INASSIGN;
+         else if ((c == ' ') || (c == '\t') || (c == '\n') || (c == '\r'))
+           save = FALSE;
+         else if (c == '{')
+         { save = FALSE;
+           state = INCOMMENT;
+         }
+         else if (c == '+')
+           state = INC;
+         else if (c == '-')
+           state = DEC;
+         else if (c == '=')
+           state = REEQ;
+         else if (c == '<')
+           state = LTS;
+         else if (c == '>')
+           state = RTS;
+         else
+         { state = DONE;
+           switch (c)
+           { case EOF:
+               save = FALSE;
+               currentToken = ENDFILE;
+               break;
+             case '=':
+               currentToken = EQ;
+               break;
+             case '?':
+               currentToken = CHOOSE;
+               break;
+             case '&':
+               currentToken = CONNECT;
+               break;
+             case '|':
+               currentToken = OR;
+               break;
+             case '#':
+               currentToken = CLOSURE;
+               break;
+             case '*':
+               currentToken = TIMES;
+               break;
+             case '/':
+               currentToken = OVER;
+               break;
+             case '(':
+               currentToken = LPAREN;
+               break;
+             case ')':
+               currentToken = RPAREN;
+               break;
+             case ';':
+               currentToken = SEMI;
+               break;
+             case '%':
+               currentToken = MOD;
+               break;
+             case '^':
+               currentToken = POWER;
+               break;
+             default:
+               currentToken = ERROR;
+               break;
+           }
+         }
+         break;
+       case INCOMMENT:
+         save = FALSE;
+         if (c == EOF)
+         { state = DONE;
+           currentToken = ENDFILE;
+         }
+         else if (c == '}') 
+          state = START;
+         break;
+       case INASSIGN:
+         state = DONE;
+         if (c == '=')
+           currentToken = ASSIGN;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = ERROR;
+         }
+         break;
+       case REEQ:
+         state = DONE;
+         if (c == '=')
+           currentToken = REASSIGN;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = EQ;
+         }
+         break;
+       case LTS:
+         state = DONE;
+         if (c == '=')
+           currentToken = LTEQ;
+         else if (c == '>')
+           currentToken = NEQ;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = LT;
+         }
+         break;
+       case RTS:
+         state = DONE;
+         if (c == '=')
+           currentToken = RTEQ;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = RT;
+         }
+         break;
+       case INNUM:
+         if (!isdigit(c))
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           state = DONE;
+           currentToken = NUM;
+         }
+         break;
+       case INID:
+         if (!isalpha(c))
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           state = DONE;
+           currentToken = ID;
+         }
+         break;
+       case INC:
+         state = DONE;
+         if (c == '+')
+           currentToken = INCREASE;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = PLUS;
+         }
+         break;
+       case DEC:
+         state = DONE;
+         if (c == '-')
+           currentToken = DECREASE;
+         else
+         { /* backup in the input */
+           ungetNextChar();
+           save = FALSE;
+           currentToken = PLUS;
+         }
+         break;
+       case DONE:
+       
+       default: /* should never happen */
+         fprintf(listing,"Scanner Bug: state= %d\n",state);
+         state = DONE;
+         currentToken = ERROR;
+         break;
+     }
+     if ((save) && (tokenStringIndex <= MAXTOKENLEN))
+       tokenString[tokenStringIndex++] = (char) c;
+     if (state == DONE)
+     { tokenString[tokenStringIndex] = '\0';
+       if (currentToken == ID)
+         currentToken = reservedLookup(tokenString);
+     }
+   }
+   if (TraceScan) {
+     fprintf(listing,"\t%d: ",lineno);
+     printToken(currentToken,tokenString);
+   }
+   return currentToken;
+} /* end getToken */
+
---
+++ lab3/GUI/scan.h
@@ -0,0 +1,25 @@
+#ifndef _SCAN_H_
+#define _SCAN_H_
+
+#include "globals.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* MAXTOKENLEN is the maximum size of a token */
+#define MAXTOKENLEN 40
+
+/* tokenString array stores the lexeme of each token */
+extern char tokenString[MAXTOKENLEN+1];
+
+/* function getToken returns the
+ * next token in source file
+ */
+TokenType getToken(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // _SCAN_H_

de2dfe28a1e6baff19aefd8ce195b6502a922897
King3212 <116470803+King3212@users.noreply.github.com>
2025-01-19 17:21:30 +0800

    add something forget

--- project/grammarAnalyzer/main.cpp
+++ project/grammarAnalyzer/main.cpp
@@ -4,7 +4,7 @@
 
 int main(){
     vector<string> ignoreSigns = readFile("./input/ignore.txt");
-    Parser a = Parser("./input/edge.txt",ignoreSigns);
+    Parser a = Parser("./input/edge.txt",ignoreSigns,"./input/signMap.txt");
     printf("finish reading edges\n");
     a.parse("./input/program.prm");
     return 0;
